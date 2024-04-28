791331513709e3c8d146ee4ae793a5c36d4da214
b22robhe <96245488+b22robhe@users.noreply.github.com>
2024-04-29 00:44:01 +0200

    Updated Mountains.java added constuctors added getters and setters added @Override on to.String

--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -3,13 +3,33 @@ package com.example.networking;
 @SuppressWarnings("WeakerAccess")
 public class Mountain {
 
-    private String title;
+    private String name;
+    private String location;
+    private int height;
 
-    public Mountain(String title) {
-        this.title = title;
+
+    public Mountain() {
+        name="No Name";
+        location="No Location";
+        height=-1;
     }
 
-    public String getTitle() {
-        return title;
+    public Mountain(String name, String location, int height) {
+        this.name = name;
+        this.location = location;
+        this.height= height;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    @Override
+    public String toString(){
+        return name;
     }
 }
