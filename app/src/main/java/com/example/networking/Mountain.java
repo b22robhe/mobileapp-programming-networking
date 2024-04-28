d2b470a0bf85c058f6f052bc368ee4beb1b2a898
b22robhe <96245488+b22robhe@users.noreply.github.com>
2024-04-28 22:36:03 +0200

    Mountain Activity added layout for mountain item added java code for mountain item

---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,15 @@
+package com.example.networking;
+
+@SuppressWarnings("WeakerAccess")
+public class Mountain {
+
+    private String title;
+
+    public Mountain(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
